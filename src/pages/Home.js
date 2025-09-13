a958ecb372db8d138c9a5476f73d7a0ca231be85
Satwik Panda <satwikpand83@gmail.com>
2025-09-13 14:15:28 +0530

    webvitals added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,7 @@ const Home = () => {
     return (
         <div className="min-h-screen bg-white text-zinc-900 dark:bg-zinc-950 dark:text-zinc-100">
             <header className="sticky top-0 z-10 border-b border-zinc-200 bg-white/80 backdrop-blur dark:border-zinc-800 dark:bg-zinc-950/80">
-            <SearchBar onSearch={setQuery} />
-                
+                <SearchBar onSearch={setQuery} />   
             </header>
             <main className="mx-auto max-w-6xl">
                 <ProductList query={query} />
